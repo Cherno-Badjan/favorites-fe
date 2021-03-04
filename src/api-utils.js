324c5e8e5c0f0eb3b4640d9e77422be458169d23
Cherno Badjan <badjancherno@gmail.com>
2021-03-04 12:58:59 -0800

    API fucntions

--- src/api-utils.js
+++ src/api-utils.js
@@ -13,3 +13,25 @@ export async function loginUser(email, password) {
     return response.body
 }
 
+export async function searchRestaurants(location, token) {
+    const response = await request.get(`${URL}/api/search?${location}`).set('Authorization', token)
+
+    return response.body
+}
+
+export async function getFavorites(token) {
+    const response = await request.get(`${URL}/api/favorites`).set('Authorization', token)
+
+    return response.body
+}
+export async function addFavorite(restaurant, token) {
+    const response = await request.post(`${URL}/api/favorites`).set('Authorization', token).send(restaurant)
+
+    return response.body
+}
+export async function deleteFavorite(restaurantId, token) {
+    const response = await request.delete(`${URL}/api/favorites/${restaurantId}`).set('Authorization', token)
+
+    return response.body
+}
+
