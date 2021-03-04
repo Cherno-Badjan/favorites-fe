ac1ce524d7556012fd1bdacb95a6e1790a60f2bb
Cherno Badjan <badjancherno@gmail.com>
2021-03-04 13:38:07 -0800

    App.js completed

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react'
 // import HomePage from './Home/HomePage.js';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch, } from 'react-router-dom';
-// import FavoritesPage from './FavoritesPage/FavoritesPage.js';
+import FavoritesPage from './FavoritesPage/FavoritesPage.js';
 import Header from './Components/Header.js';
-// import SearchPage from './SearchPage/SearchPage.js';
-// import PrivateRouter from './Components/PrivateRouter.js';
+import SearchPage from './SearchPage/SearchPage.js';
+import PrivateRouter from './Components/PrivateRouter.js';
 import SignUpPage from './AuthPages/SignUpPage.js';
 import { getUserFromLs, putUserInLs } from './localStorageUtils';
 import HomePage from './Home/HomePage';
@@ -21,13 +21,21 @@ export default class App extends Component {
 
     putUserInLs(user);
   }
+
+  handleLogout = () => {
+    this.handleUserChange();
+  }
   render() {
+    const { user } = this.state;
     return (
       <div>
         <Router>
-          <Header />
+          <Header user={this.state.user}
+            handleLogout={this.handleLogout} />
           <Switch>
             <Route path="/" exact render={(routerProps) => <HomePage {...routerProps} />} />
+            <Route path="/search" exact render={(routerProps) => <SearchPage {...routerProps} user={this.state.user} />} />
+            <PrivateRouter path="/favorites" exact token={user && user.token} render={(routerProps) => <FavoritesPage user={this.state.user} {...routerProps} />} />
             <Route path="/signup" exact render={(routerProps) => <SignUpPage handleUserChange={this.handleUserChange} {...routerProps} />} />
             <Route path="/login" exact render={(routerProps) => <LoginPage handleUserChange={this.handleUserChange} {...routerProps} />} />
           </Switch>
